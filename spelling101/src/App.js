4aaa5ba63ee3159e18f74bfe332d9e3e77a08e7d
showboat051 <rashad.davis87@yahoo.com>
2019-02-13 22:07:11 -0600

    got respone from API

--- spelling101/src/App.js
+++ spelling101/src/App.js
@@ -5,60 +5,33 @@ import Form from './components/Form';
 
 class App extends Component {
   
+  state = {
+    loading: true
+  }
  
-
-  // state = {
-  //   meta: undefined,
-  //   syn_list: undefined,
-  //   wsls: undefined,
-  //   rel_list: undefined,
-  //   list: undefined,
-  //   error: undefined,
-  // };
-
-  // getWord = async (e) => {
-  //   e.preventdefault();
-  //   const word = e.target.elements.word.value;
-
-  //   // const api_key = "49100cd7-ffd4-4401-a19c-f3d89af225bb";
-  //   // https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb
-  //   const api_call = await fetch('https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=49100cd7-ffd4-4401-a19c-f3d89af225bb');
-  //   const data = await api_call.json();
-
-
-
-  //   if (word) {
-  //     console.log (data);
-  //     this.setState({
-  //       meta: data.target,
-  //     });
-  //   } else {
-  //     console.log(data);
-  //     this.setState({
-  //       meta: undefined,
-  //       error: "You didn't enter a word."
-  //     });
-  //   }
-  // }
+ async componentDidMount () {
+    const url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
   
-    constructor() {
-      super();
-      this.state = { data: [] };
-    }
-    componentDidMount() {
-      fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb`)
-        .then(res => res.json())
-        .then(json => this.setState({ data: json }));
-        
-    }
+
+
+    
+    
+
+    // componentDidMount() {
+    //   fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb`)
+    //     .then(res => res.json())
+    //     .then(json => this.setState({ data: json }));
+          
+    // }
 
   render() {
     return (
       <div>
-        {/* <Form getWord={this.getWord} /> */}
-          
-        <Home />
-        <p>Enter your word here </p> <Form />
+        {this.state.loading ? <div>loading...</div> : <div>word...</div>}
       </div>
     );
   }
