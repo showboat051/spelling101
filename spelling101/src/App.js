f266a8768ff4bcb9a212c644cc292f9ac20acb74
showboat051 <rashad.davis87@yahoo.com>
2019-02-16 00:06:37 -0600

    rendering works, need to learn how API works

--- spelling101/src/App.js
+++ spelling101/src/App.js
@@ -6,32 +6,23 @@ import Form from './components/Form';
 class App extends Component {
   
   state = {
-    loading: true
-  }
+    loading: true,
+    word: null,
+  };
  
  async componentDidMount () {
     const url = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb';
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    this.setState({word: data[0], loading: false });
+    console.log(data[0].meta);
   }
   
-
-
-    
-    
-
-    // componentDidMount() {
-    //   fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/test?key=49100cd7-ffd4-4401-a19c-f3d89af225bb`)
-    //     .then(res => res.json())
-    //     .then(json => this.setState({ data: json }));
-          
-    // }
-
   render() {
     return (
       <div>
-        {this.state.loading ? <div>loading...</div> : <div>word...</div>}
+        {this.state.loading || !this.state.word ? (
+        <div>loading...</div>) : <div>{this.state.word.meta.id}</div>}
       </div>
     );
   }
